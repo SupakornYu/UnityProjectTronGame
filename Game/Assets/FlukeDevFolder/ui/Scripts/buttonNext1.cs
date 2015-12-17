ff792b008da425b6396ac74b1f53b444e5168a7f
Panat523 <racca_f@hotmail.com>
2015-12-17 19:36:50 +0700

    ui

---
+++ Game/Assets/FlukeDevFolder/ui/Scripts/buttonNext1.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonNext1 : MonoBehaviour{
+	
+	void Start () {
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	
+	}
+	public void changescene(){
+		Application.LoadLevel ("Player2");
+
+	}
+
+}
---
+++ Game/Assets/FlukeDevFolder/ui/Scripts/start.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class start : MonoBehaviour{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void changesceneStart(){
+		Application.LoadLevel ("Player1");
+
+	}
+	public void changesceneRanking(){
+		Application.LoadLevel ("Ranking");
+
+	}
+	public void changesceneCredit(){
+		Application.LoadLevel ("Credit");
+
+	}
+	public void buttonEffect(){
+		transform.Rotate (new Vector3 (0.0f, 1.0f, 0.0f), 45.0f);
+	}
+
+}

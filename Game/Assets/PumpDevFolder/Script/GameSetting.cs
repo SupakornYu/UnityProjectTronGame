e6fbba66bd2bf85e5f4059696bd34de4b1b529aa
SupakornYu <supakorn_pump@hotmail.com>
2015-12-17 19:26:23 +0700

    -Add GameSettingClass class and create static variable to sync health and speed

---
+++ Game/Assets/PumpDevFolder/Script/GameSetting.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSetting : MonoBehaviour {
+
+	public static int Player1Health;
+	public static int Player2Health;
+	public static int Player1Speed;
+	public static int Player2Speed;
+
+}
